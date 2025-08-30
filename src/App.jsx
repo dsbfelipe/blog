c9e7bc0b1d3b467f10dd36df9e58317a22a40579
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-08-30 04:45:31 -0300

    chore: create file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <>
-      <></>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 };
 
