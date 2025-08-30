1941226fe9c11a81b97d24dcd528779d28d038c6
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-08-29 23:34:59 -0300

    chore: add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
@@ -12,6 +13,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    tailwindcss(),
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
